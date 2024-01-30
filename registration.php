84dcffb1b74663b0524fa86f8af48a14584d428a
nilcoder09 <144077609+nilcoder09@users.noreply.github.com>
2024-01-30 10:04:16 -0600

    php update

--- registration.php
+++ registration.php
@@ -4,18 +4,19 @@ if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
 
-if ($_SERVER["REQUEST_METHOD"] == "POST") {
+if ($_SERVER["REQUEST_METHOD"] == "post") {
     $username = $_POST['username'];
     $password = password_hash($_POST['password'], PASSWORD_BCRYPT); // Hash the password
     $email = $_POST['email'];
     $phone = $_POST['phone'];
 
-    $sql = "INSERT INTO project1 (username, password, email, phone) VALUES ('$username', '$password', '$emial', '$phone')";
+    $sql = "INSERT INTO project1 (username, passwordnew, email, phone) VALUES ('$username', '$password', '$email', '$phone')";
 
     if ($conn->query($sql) === TRUE) {
         echo "Registration successful!";
     } else {
-        echo "Error: " . $sql . "<br>" . $conn->error;
+        // echo "Error: " . $sql . "<br>" . $conn->error;
+        echo "error";
     }
 }
 
